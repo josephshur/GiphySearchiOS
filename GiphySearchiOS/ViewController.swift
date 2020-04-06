a935ff5168f02b01f38262f98e1daeb935b582eb
Joseph Shur <joehshur@gmail.com>
2020-04-05 18:10:31 -0700

    Initial Commit

---
+++ GiphySearchiOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GiphySearchiOS
+//
+//  Created by Joseph Shur on 4/5/20.
+//  Copyright © 2020 Joseph Shur. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
